21d831a9ec9b2888bc992bce6a37a9b5a37d1952
Charles2k16 <charlesprogram@gmail.com>
2022-01-22 22:58:39 +0000

    set up project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="hea">Hello there</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" scoped>
+.hea {
+  color: red;
+}
+</style>
