6626ab58e96a1c07cfa5ee4ab9debb853f1857c1
Charles2k16 <charlesprogram@gmail.com>
2022-01-23 02:45:41 +0000

    selector

---
+++ components/CurrencySelector.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="dropdown">
+      <span class="currency d-flex">
+        <img src="../assets/data/united-states.png" alt="us" width="22px" />
+        <span class="label mx-15">US Dollar</span>
+        <img src="/down.png" alt="arr" width="20px" />
+      </span>
+
+      <!-- The content -->
+      <div class="dropdown__content">
+        <ul>
+          <li>
+            <img src="../assets/data/united-states.png" alt="us" width="20px" />
+            USD - US Dollar
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurrencySelector',
+}
+</script>
+
+<style lang="scss" scoped>
+.dropdown {
+  position: relative;
+
+  .currency {
+    align-items: center;
+
+    .label {
+      color: var(--sec_grey);
+      font-size: 1.3rem;
+    }
+  }
+  .dropdown__content {
+    display: none;
+    left: 0;
+    padding-top: 4px;
+    position: absolute;
+    top: 100%;
+    background-color: #fff;
+    z-index: 9999;
+    height: 200px;
+    width: 200px;
+  }
+}
+
+.dropdown:hover .dropdown__content {
+  display: block;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,102 @@
 <template>
-  <div>
-    <h1 class="hea">Hello there</h1>
+  <div class="converter_container d-flex-justify-center">
+    <div class="conv_wrapper">
+      <div class="conv_head mx-30">
+        <span class="link active">Converter</span>
+
+        <span class="link">Calculator</span>
+      </div>
+
+      <div class="mt-20 conv_title mx-30">
+        <h1 class="white">Currency</h1>
+
+        <div>
+          <span>12th Jan 2022</span>
+          <img src="/clock.png" alt="time" width="12px" />
+        </div>
+      </div>
+
+      <hr class="rule mt-20" />
+
+      <div class="mt-20 mx-30 exchange_container">
+        <h3>CURRENCY I HAVE</h3>
+        <p class="mt-0">I have this much to exchange</p>
+
+        <hr class="rule mt-20" />
+
+        <div class="exchange_selector mt-20 d-flex-justify-end">
+          <div>
+            <CurrencySelector />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {}
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.hea {
-  color: red;
+.converter_container {
+  height: 100vh;
+  width: 100vw;
+  padding: 50px 0;
+
+  .conv_wrapper {
+    max-width: 450px;
+    width: 100%;
+    background: #121212;
+    border-radius: 40px;
+    padding: 20px 0;
+
+    .conv_head {
+      height: 50px;
+      display: flex;
+      align-items: center;
+
+      .link {
+        color: var(--light_grey);
+        font-size: 1rem;
+        margin-right: 40px;
+      }
+      .active {
+        color: var(--primary);
+        font-size: 1.2rem;
+        font-weight: bold;
+      }
+    }
+
+    .conv_title {
+      height: 30px;
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+
+      h1 {
+        font-size: 2rem;
+      }
+
+      span {
+        color: var(--sec_grey);
+        font-size: 1rem;
+      }
+    }
+
+    .exchange_container {
+      h3 {
+        color: rgb(192, 189, 189);
+      }
+      p {
+        color: var(--light_grey);
+      }
+    }
+  }
 }
 </style>
